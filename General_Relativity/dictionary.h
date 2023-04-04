8a75c6ecc632ff8b52611d787a0ab238d057e03b
Luis Silfredo Cuevas <97488032+whees@users.noreply.github.com>
2023-04-04 00:19:06 -0400

    dlete cictionara

--- General_Relativity/dictionary.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef float (*fourParamFunc) (float*);
-
-
-float squared(float x);
-float derivitive(fourParamFunc func, int index, float* abcd);
-float det4(fourParamFunc** opps, float* abcd);
-float det3(fourParamFunc** opps, float* abcd);
